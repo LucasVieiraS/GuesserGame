5845421d87c9ee49690814394271863e4710817e
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-10-08 12:54:42 -0300

    refactor: code restructured
    - Switched to OOP
    - Added Score linked to LocalStorage
    - Added Hints
    - Fixed Minor Bugs

--- script.js
+++ script.js
@@ -1,29 +1,238 @@
-class Tile {
-    constructor(id, column) {
-        this.id = id;
-        this.column = column;
-        this.element = document.getElementById(id);
-    }
-    fillTile(newKey) {
-        this.element.value = newKey;
-    }
-}
+const ALPHABET = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const API_URL = 'https://api.datamuse.com/words?max=1000&sp=?????';
 
-class Tiles {
-    constructor(tiles) {
-        this.tiles = tiles;
-    }
-    getTileDataFromID(ID) {
-        return this.tiles[ID];
-    }
-}
+let rows = [];
+let currentRow = 1;
+let currentWord;
+let currentWordScore;
+
+let typingState = false;
+let hintsLeft = 2;
+let totalScore = localStorage.getItem("score") || 0;
+totalScore = parseInt(totalScore);
 
 class Row {
-    constructor(id) {
-        this.id = id;
-        this.tiles = new Tiles(this.getTilesInRow())
+    constructor(element) {
+        this.element = element
+        this.id = parseInt(element.id.replace('row-', ''));
+        this.tiles = this.getTilesInRow();
+    }
+    goToNextRow() {
+        currentRow += 1;
+        let nextRowObject = rows[currentRow - 1];
+        nextRowObject.tiles.forEach((data) => {
+            const element = data.element;
+            element.removeAttribute("disabled");
+        })
+        goToNextInput(this.tiles[this.tiles.length-1].element.id)
+    }
+    verifyWord() {
+        let correctLetters = 0;
+        for (let i = 0; i < this.tiles.length; i++) {
+            let tileData = this.getTileDataFromIndex(i);
+            let tileElement = tileData.element;
+            let tileValue = tileElement.value;
+            if (!currentWord.includes(tileValue)) {
+                tileElement.classList.add('tile-wrong');
+            } else if (currentWord[i] == tileValue) {
+                correctLetters += 1;
+                tileElement.classList.add('tile-right');
+            } else {
+                tileElement.classList.add('tile-warning');
+            }
+            tileElement.disabled = "true";
+        }
+        if (correctLetters < 5) {
+            if (currentRow > 5) {
+                return endGame("lose")
+            }
+            return this.goToNextRow();
+        }
+        totalScore += currentWordScore || 1;
+        localStorage.setItem("score", totalScore);
+        return endGame("win")
+    }
+    isFilled() {
+        let filledTiles = 0;
+        this.tiles.forEach((data) => {
+            if (data.element.value != "") {
+                filledTiles += 1;
+            }
+        })
+        return filledTiles == this.tiles.length;
     }
     getTilesInRow() {
-        
+        let arrayOfTiles = [];
+        let currentIndex = 0;
+        Array.from(this.element.children).forEach((tileElement) => {
+            const tileId = parseInt(tileElement.id);
+            arrayOfTiles[currentIndex] = {
+                key: tileId,
+                element: tileElement
+            }
+            tileElement.value = ""
+            tileElement.classList.remove("tile-right")
+            tileElement.classList.remove("tile-wrong")
+            tileElement.classList.remove("tile-warning")
+            if (this.id != currentRow) {
+                tileElement.disabled = "true";
+            } else {
+                tileElement.removeAttribute("disabled")
+            }
+            currentIndex += 1;
+        })
+        return arrayOfTiles;
+    }
+    getTileDataFromID(ID) {
+        this.tiles.forEach((data) => {
+            if (data.key == ID) {
+                return data;
+            }
+        })
+        return this.tiles[ID];
+    }
+    getTileDataFromIndex(Index) {
+        return this.tiles[Index]
+    }
+    getLastTile() {
+        return this.tiles[this.tiles.length-1]
+    }
+    getClosestClearTile() {
+        let result;
+        for (let i = 0; i < this.tiles.length; i++) {
+            let tileData = this.getTileDataFromIndex(i);
+            let tileElement = tileData.element;
+            if (tileElement.value == "") {
+                result = tileElement;
+                break
+            }
+        }
+        return result;
+    }
+}
+
+async function getWordFromApi() {
+    let response = await fetch(API_URL);
+    let wordList = await response.json();
+
+    const data = wordList[Math.floor(Math.random() * wordList.length - 1)];
+
+    currentWordScore = data.score
+    currentWord = data.word.toUpperCase();
+
+    document.getElementById("word").innerText = currentWord;
+
+    if (currentWord.includes(" ")) getWordFromApi();
+}
+
+async function iterateWithCallback(array, callback) {
+    array.forEach((argument) => {
+         callback(argument)
+    })
+}
+
+function setTypingState(bool) {
+    typingState = bool;
+}
+
+function addKey(key) {
+    const currentRowObject = rows[currentRow - 1];
+    const emptyTile = currentRowObject.getClosestClearTile();
+    if (emptyTile) {
+        emptyTile.value = key
+    }
+}
+
+function goToNextInput(id) {
+    let nextId = parseInt(id) + 1;
+    let nextInputElement = document.getElementById(nextId);
+    let currentRowElement = document.getElementById(`row-${currentRow}`)
+    if (currentRowElement.id != `row-${currentRow}`) return console.log("Not on the same row.");
+    if (!nextInputElement) return
+    nextInputElement.select();
+    nextInputElement.focus();
+}
+
+function goToPreviousInput(id) {
+    let previousId = parseInt(id) - 1;
+    previousId = previousId < 1 && 1 || previousId;
+    let previousInputElement = document.getElementById(previousId);
+    let currentRowElement = document.getElementById(`row-${currentRow}`)
+    if (currentRowElement.id != `row-${currentRow}`) return console.log("Not on the same row.");
+    previousInputElement.select();
+    previousInputElement.focus();
+}
+
+function onChange(id, value) {
+    if (value == "") {
+        goToPreviousInput(id);
+    }
+}
+
+function keyDown(event) {
+    const currentRowObject = rows[currentRow - 1];
+    const keyName = event.key.toUpperCase();
+    if (keyName == "ENTER") {
+        if (currentRowObject.isFilled()) {
+            currentRowObject.verifyWord();
+        } else {
+            alert(`Row ${currentRow} is not filled.`)
+        }
+    } else if (ALPHABET.includes(keyName) && !typingState) {
+        const emptyTile = currentRowObject.getClosestClearTile();
+        if (emptyTile) {
+            emptyTile.focus();
+            emptyTile.select();
+        }
+        addKey(keyName)
+    }
+}
+
+function loadGame() {
+    rows = [];
+    const scoreElement = document.getElementById("score");
+    scoreElement.innerText = `SCORE: ${totalScore}`;
+    function createRow(rowElement) {
+        rows.push(
+            new Row(rowElement)
+        )
+    }
+    getWordFromApi();
+    const rowElements = Array.from(document.getElementsByClassName("column"));
+    iterateWithCallback(rowElements, createRow)    
+    document.addEventListener('keydown', keyDown);
+}
+
+function endGame(status) {
+    const modal = document.getElementById("modal")
+    const resultElement = document.getElementById("result");
+    const scoreElement = document.getElementById("score");
+    resultElement.innerText = status;
+    resultElement.style = `color: rgb(${status == "win" && '0, 255, 0' || '255, 0, 0'})`;
+    scoreElement.innerText = `SCORE: ${totalScore}`;
+    modal.style = "";
+}
+
+function restartGame() {
+    currentRow = 1;
+    const modal = document.getElementById("modal");
+    modal.style = "visibility: hidden;";
+    setTimeout(() => {
+        loadGame();
+        const firstTile = document.getElementById("1");
+        firstTile.select();
+        firstTile.focus();
+    }, 100);
+}
+
+function takeHint() {
+    const totalHints = document.getElementById("total-hints");
+    hintsLeft -= 1
+    if (hintsLeft >= 0) {
+        totalHints.innerText = `${hintsLeft} HINTS LEFT`
+        alert(`Word contains the letter ${currentWord[Math.floor(Math.random() * 5)]}`)
+        if (hintsLeft == 0) {
+            totalHints.style.color = "rgb(255, 0, 0)";
+        }
     }
 }
